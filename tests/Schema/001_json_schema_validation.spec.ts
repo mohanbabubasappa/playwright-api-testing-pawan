72e119f7f3a0fbb218d485679646995218c58623
mohanbabubasappa <mohanbabu777ms@gmail.com>
2026-09-01 20:31:42 +0530

    added

--- tests/Schema/001_json_schema_validation.spec.ts
+++ tests/Schema/001_json_schema_validation.spec.ts
@@ -11,74 +11,68 @@ ajv.compile(schema) returns a validator function.
 validate(data) checks if the response matches the schema.
 */
 
-import { test, expect } from '@playwright/test';
-import Ajv from 'ajv';
+import { test, expect } from "@playwright/test";
+import Ajv from "ajv";
 
 test("Schema validation", async ({ request }) => {
-    //step 1: send request and get the response
+  //step 1: send request and get the response
 
-    const response = await request.get('https://mocktarget.apigee.net/json');
-    const responsebody = await response.json();
-    console.log(responsebody)
+  const response = await request.get("https://mocktarget.apigee.net/json");
+  const responsebody = await response.json();
+  console.log(responsebody);
 
-    //step 2: define the schema
+  //step 2: define the schema
 
-    const schema = {
-        "type": "object",
-        "properties": {
-            "firstName": {
-                "type": "string"
-            },
-            "lastName": {
-                "type": "string"
-            },
-            "city": {
-                "type": "string"
-            },
-            "state": {
-                "type": "string"
-            }
-        },
-        "required": [
-            "firstName",
-            "lastName",
-            "city",
-            "state"
-        ]
-    }
+  const schema = {
+    type: "object",
+    properties: {
+      firstName: {
+        type: "string",
+      },
+      lastName: {
+        type: "string",
+      },
+      city: {
+        type: "string",
+      },
+      state: {
+        type: "string",
+      },
+    },
+    required: ["firstName", "lastName", "city", "state"],
+  };
 
-
-    //step 3: check response against schema
-    const ajv=new Ajv()
-    const validate= ajv.compile(schema) //returns a validator function
-     const isValid= validate(responsebody)
-    expect(isValid).toBeTruthy() // assertion
-})
-
-//Example 2:  
-
-test.only('Validate JSON response with schema2', async ({ request }) => {
-    const response = await request.get('https://jsonplaceholder.typicode.com/posts/1');
-    const responsebody = await response.json();
-
-    // Define the JSON schema
-    const schema = {
-        type: 'object',
-        properties: {
-            userId: { type: 'integer' },
-            id: { type: 'integer' },
-            title: { type: 'string' },
-            body: { type: 'string' },
-        },
-        required: ['userId', 'id', 'title', 'body'],
-        additionalProperties: false,
-    };
-
-    const ajv = new Ajv();   // Initialize Ajv
-    const validate = ajv.compile(schema); //returns a validator function
-    const isValid = validate(responsebody); //checks if the response matches the schema
-
-    expect(isValid).toBeTruthy();
-
+  //step 3: check response against schema
+  const ajv = new Ajv();
+  const validate = ajv.compile(schema); //returns a validator function
+  const isValid = validate(responsebody);
+  expect(isValid).toBeTruthy(); // assertion
 });
 
+//Example 2:
+
+test("Validate JSON response with schema2", async ({ request }) => {
+  const response = await request.get(
+    "https://jsonplaceholder.typicode.com/posts/1",
+  );
+  const responsebody = await response.json();
+
+  // Define the JSON schema
+  const schema = {
+    type: "object",
+    properties: {
+      userId: { type: "integer" },
+      id: { type: "integer" },
+      title: { type: "string" },
+      body: { type: "string" },
+    },
+    required: ["userId", "id", "title", "body"],
+    additionalProperties: false,
+  };
+
+  const ajv = new Ajv(); // Initialize Ajv
+  const validate = ajv.compile(schema); //returns a validator function
+  const isValid = validate(responsebody); //checks if the response matches the schema
+
+  expect(isValid).toBeTruthy();
+});
